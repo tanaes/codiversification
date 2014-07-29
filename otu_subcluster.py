bacccf063ccca2fda4289486c9d82cf294007b04
Yoshiki Vázquez Baeza <yoshiki89@gmail.com>
2014-07-29 09:36:33 -0600

    Add PEP8 fixes

--- otu_subcluster.py
+++ otu_subcluster.py
@@ -3,38 +3,39 @@
 
 __author__ = "Jon Sanders"
 __copyright__ = "Copyright 2012"
-__credits__ = ["Rob Knight", "Justin Kuczynski","Jesse Stombaugh","Jon Sanders"]
+__credits__ = ["Rob Knight", "Justin Kuczynski",
+               "Jesse Stombaugh", "Jon Sanders"]
 __license__ = "GPL"
 __version__ = "1.4.0"
 __maintainer__ = "Jon Sanders"
 __email__ = "jonsan@gmail.com"
 __status__ = "Experimental"
 
-#Nate's test edit
-#Jon's test addition
+# Nate's test edit
+# Jon's test addition
 
-#importing modules 
+# importing modules
 from sys import exit, stderr, stdout
 from subcluster import *
 
-import os, sys
+import os
+import sys
 
 from qiime.util import make_option
 from qiime.util import load_qiime_config, parse_command_line_parameters,\
- get_options_lookup
+    get_options_lookup
 from qiime.parse import parse_qiime_parameters, parse_taxonomy
 import os.path
 import os
 
 
-
-
 options_lookup = get_options_lookup()
 
-#otu_subcluster_v0.py
-script_info={}
-script_info['brief_description']="""Subcluster sequences within an OTU table"""
-script_info['script_description']="""This workflow script takes the constituent 
+# otu_subcluster_v0.py
+script_info = {}
+script_info[
+    'brief_description'] = """Subcluster sequences within an OTU table"""
+script_info['script_description'] = """This workflow script takes the constituent 
 sequences of OTUs from an OTU map ('parent OTUs') and re-clusters them at a higher
 percent ID (into 'child OTUs') using typical QIIME workflow commands, outputting
 new OTU tables, maps, and rep_set files (including an alignment and tree) for 
@@ -43,13 +44,13 @@ each OTU.
 Changes to the default QIIME workflow behavior must be specified by a parameters 
 file. If, for example, you want to subcluster OTU sequences at 99% ID, you would
  modify the pick_otus:similarity line to 0.99."""
-script_info['script_usage']=[]
+script_info['script_usage'] = []
 
 
-script_info['script_usage'].append(("","""Subcluster all sequences: ""","""%prog 
+script_info['script_usage'].append(("", """Subcluster all sequences: """, """%prog 
 -i otu_map.txt -o subclustered_otus -f seqs.fna -p subcluster_params.txt 
 -t Example_input/otu_table.txt"""))
-script_info['output_description']="""Output is standard QIIME otu tables, maps, 
+script_info['output_description'] = """Output is standard QIIME otu tables, maps, 
 and rep_set fasta files, one per original (parent) OTU. Files are all output to 
 a new directory, specified by -o or --output_dir. These files follow the naming 
 convention pOTU#_[fasta basename]_[filetype]. For example, the sequences which 
@@ -57,40 +58,42 @@ constitute parent OTU #90 will end up in a file called '90_seqs.fasta.' Clusters
  for these child OTUs will be defined in 90_seqs_otus.txt, and summarized in an 
  OTU table in 90_seqs_otu_table.txt."""
 
-script_info['required_options']=[\
- make_option('-i','--otu_map_fp',\
-			help='the input OTU map file [REQUIRED]'),\
- make_option('-o','--output_dir',\
-			help='path to output directory [REQUIRED]'),\
- make_option('-f','--fasta_fp',\
-			help='path to sequence fasta [REQUIRED]'),\
- make_option('-p','--parameter_fp',\
-			help='path to parameters file [REQUIRED]'),\
- make_option('-t','--otu_table_fp',\
-			help='path to OTU table file [REQUIRED]')		
+script_info['required_options'] = [
+    make_option('-i', '--otu_map_fp',
+                help='the input OTU map file [REQUIRED]'),
+    make_option('-o', '--output_dir',
+                help='path to output directory [REQUIRED]'),
+    make_option('-f', '--fasta_fp',
+                help='path to sequence fasta [REQUIRED]'),
+    make_option('-p', '--parameter_fp',
+                help='path to parameters file [REQUIRED]'),
+    make_option('-t', '--otu_table_fp',
+                help='path to OTU table file [REQUIRED]')
 ]
-script_info['optional_options']=[
+script_info['optional_options'] = [
 
- make_option('--force',action='store_true',\
-		dest='force',help='Force overwrite of existing output directory'+\
-		' (note: existing files in output_dir will not be removed)'+\
-		' [default: %default]'),options_lookup['jobs_to_start_workflow']
+    make_option('--force', action='store_true',
+                dest='force', help='Force overwrite of existing output directory' +
+                ' (note: existing files in output_dir will not be removed)' +
+                ' [default: %default]'), options_lookup['jobs_to_start_workflow']
 ]
 
 script_info['version'] = __version__
 
+
 def main():
 
-	option_parser, opts, args = parse_command_line_parameters(**script_info)
-	
-	output_dir = opts.output_dir
-	otu_map_fp = opts.otu_map_fp
-	otu_table_fp = opts.otu_table_fp
-	fasta_fp = opts.fasta_fp
-	parameter_fp = opts.parameter_fp
-	force = opts.force
-	otu_subcluster(output_dir,otu_map_fp,otu_table_fp,fasta_fp,parameter_fp,force)
+    option_parser, opts, args = parse_command_line_parameters(**script_info)
+
+    output_dir = opts.output_dir
+    otu_map_fp = opts.otu_map_fp
+    otu_table_fp = opts.otu_table_fp
+    fasta_fp = opts.fasta_fp
+    parameter_fp = opts.parameter_fp
+    force = opts.force
+    otu_subcluster(
+        output_dir, otu_map_fp, otu_table_fp, fasta_fp, parameter_fp, force)
 
 
 if __name__ == "__main__":
-	main()
+    main()
--- test_test_cospeciation.py
+++ test_test_cospeciation.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python
-#file test_filter_otus_by_sample.py
+# file test_filter_otus_by_sample.py
 
 
 # augmented & updated 2013-07-09 and 2014-05-01 by Aaron Behr
 __author__ = "Jesse Stombaugh"
-__copyright__ = "Copyright 2011, The QIIME Project" #consider project name
-__credits__ = ["Jesse Stombaugh"] #remember to add yourself
+__copyright__ = "Copyright 2011, The QIIME Project"  # consider project name
+__credits__ = ["Jesse Stombaugh"]  # remember to add yourself
 __license__ = "GPL"
 __version__ = "1.4.0"
 __maintainer__ = "Jesse Stombaugh"
@@ -19,101 +19,101 @@ from cogent.util.unit_test import TestCase, main
 from os import remove
 from cogent import LoadSeqs
 import shutil
-from qiime.filter_otus_by_sample import (filter_otus,filter_aln_by_otus,\
-                                 process_extract_samples)
+from qiime.filter_otus_by_sample import (filter_otus, filter_aln_by_otus,
+                                         process_extract_samples)
 
-# For the HommolaTests, need to get this from cogent in 
-#tests/test_maths/test_stats (actual file test_test.py)
+# For the HommolaTests, need to get this from cogent in
+# tests/test_maths/test_stats (actual file test_test.py)
 from test_test import TestsHelper
 
 ''' NOT SURE WHY IT ONLY WORKS THIS WAY '''
 #from test_cospeciation import *
-from test_cospeciation import (hommola_cospeciation_test,\
-                         get_dist, cogent_dist_to_qiime_dist)
+from test_cospeciation import (hommola_cospeciation_test,
+                               get_dist, cogent_dist_to_qiime_dist)
 
 
 class TopLevelTests(TestCase):
+
     """Tests of top-level functions"""
 
     def setUp(self):
         """define some top-level data"""
-        self.aln=[]
-        self.aln.append(('SampleA','AAAAAAAAAAAAAAA'))
-        self.aln.append(('SampleB','CCCCCCC'))
-        self.aln.append(('SampleC','GGGGGGGGGGGGGG'))
-        
-        self.otus={'0':['SampleC'],'1':['SampleC','SampleA'],'2':['SampleB',\
-                                                                  'SampleA']}
+        self.aln = []
+        self.aln.append(('SampleA', 'AAAAAAAAAAAAAAA'))
+        self.aln.append(('SampleB', 'CCCCCCC'))
+        self.aln.append(('SampleC', 'GGGGGGGGGGGGGG'))
 
-        self.prefs={}
-        self.prefs={'0':'SampleC','1':'SampleB'}
+        self.otus = {'0': ['SampleC'], '1': ['SampleC', 'SampleA'], '2': ['SampleB',
+                                                                          'SampleA']}
+
+        self.prefs = {}
+        self.prefs = {'0': 'SampleC', '1': 'SampleB'}
 
     def test_take_(self):
         """process_extract_samples: parses the cmd line and determines which
         samples should be removed"""
 
-        self.sample_to_extract='SampleA,SampleB'
-        exp1={'0':'SampleA','1':'SampleB'}
+        self.sample_to_extract = 'SampleA,SampleB'
+        exp1 = {'0': 'SampleA', '1': 'SampleB'}
 
-        obs1=process_extract_samples(self.sample_to_extract)
+        obs1 = process_extract_samples(self.sample_to_extract)
 
-        self.assertEqual(obs1,exp1)
+        self.assertEqual(obs1, exp1)
 
     def test_filter_otus(self):
         """filter_otus: determines which sequences should be removed and
         generates a new otus list"""
 
-        exp1=[('1',['SampleA']),('2',['SampleA'])]
-        obs1=filter_otus(self.otus,self.prefs)
-            
-        self.assertEqual(obs1,exp1)
+        exp1 = [('1', ['SampleA']), ('2', ['SampleA'])]
+        obs1 = filter_otus(self.otus, self.prefs)
 
-        
+        self.assertEqual(obs1, exp1)
+
     def test_filter_aln_by_otus(self):
         """filter_aln_by_otus: determines which sequences to keep and which
         sequences to remove"""
 
-        self.sample_to_extract='SampleA,SampleB'
-        exp1=[]
-        exp1.append(('SampleA','AAAAAAAAAAAAAAA'))
-        exp2=[]
-        exp2.append(('SampleB','CCCCCCC'))
-        exp2.append(('SampleC','GGGGGGGGGGGGGG'))
-        aln=LoadSeqs(data=self.aln,aligned=False)
+        self.sample_to_extract = 'SampleA,SampleB'
+        exp1 = []
+        exp1.append(('SampleA', 'AAAAAAAAAAAAAAA'))
+        exp2 = []
+        exp2.append(('SampleB', 'CCCCCCC'))
+        exp2.append(('SampleC', 'GGGGGGGGGGGGGG'))
+        aln = LoadSeqs(data=self.aln, aligned=False)
 
-        obs1,obs2=filter_aln_by_otus(aln,self.prefs)
+        obs1, obs2 = filter_aln_by_otus(aln, self.prefs)
 
-        self.assertEqual(obs1,exp1)
-        self.assertEqual(obs2,exp2)
-
+        self.assertEqual(obs1, exp1)
+        self.assertEqual(obs2, exp2)
 
     def test_cogent_dist_to_qiime_dist(self):
 
-        input_dict = {('a','b'): 4, ('a','c'): 5, ('a','d'): 6,
-            ('b','a'): 4, ('b','c'): 7, ('b','d'): 8,
-            ('c','a'): 5, ('c','b'): 7, ('c','d'): 9,
-            ('d','a'): 6, ('d','b'): 8,('d','c'): 9}
-        
+        input_dict = {('a', 'b'): 4, ('a', 'c'): 5, ('a', 'd'): 6,
+                      ('b', 'a'): 4, ('b', 'c'): 7, ('b', 'd'): 8,
+                      ('c', 'a'): 5, ('c', 'b'): 7, ('c', 'd'): 9,
+                      ('d', 'a'): 6, ('d', 'b'): 8, ('d', 'c'): 9}
+
         # RUN METHOD TO BE TESTED
         actual_output = cogent_dist_to_qiime_dist(input_dict)
 
         # Generate expected output
-        matrix_order = ['a','b','c','d']
+        matrix_order = ['a', 'b', 'c', 'd']
         expected_output = []
         for x in matrix_order:
             row = []
             for y in matrix_order:
                 if x != y:
-                    # use input tuple dist dict to populate expected output matrix
-                    row.append(input_dict[x,y])
+                    # use input tuple dist dict to populate expected output
+                    # matrix
+                    row.append(input_dict[x, y])
                 else:
-                    # input tuple dist dict doesn't store the null self-distances
-                    row.append(0.)  
+                    # input tuple dist dict doesn't store the null
+                    # self-distances
+                    row.append(0.)
             expected_output.append(row)
         expected_output = (matrix_order, array(expected_output))
 
         self.assertEqual(actual_output, expected_output)
-
 
 
 class HommolaTests(TestsHelper):
@@ -122,41 +122,39 @@ class HommolaTests(TestsHelper):
     # want to fully override it here
 
     def test_hommola_cospecitation_test(self):
-        hdist = array([[0,3,8,8,9],[3,0,7,7,8],[8,7,0,6,7],[8,7,6,0,3],[9,8,7,3,0]])
-        pdist = array([[0,5,8,8,8],[5,0,7,7,7],[8,7,0,4,4],[8,7,4,0,2],[8,7,4,2,0]])
-        matrix = array([[1,0,0,0,0],[0,1,0,0,0],[0,0,1,0,0],[0,0,0,1,0],[0,0,0,1,1]])
-        
-        # this matrix was picked because it will generate an r value that's less than 
-        # a standard deviation away from the mean of the normal distribution of r vals 
-        randomized_matrix = array([[0,0,0,1,0],[0,0,0,0,1],[1,0,0,0,0],[1,0,0,0,0],[0,0,0,0,1]])
+        hdist = array([[0, 3, 8, 8, 9], [3, 0, 7, 7, 8], [
+                      8, 7, 0, 6, 7], [8, 7, 6, 0, 3], [9, 8, 7, 3, 0]])
+        pdist = array([[0, 5, 8, 8, 8], [5, 0, 7, 7, 7], [
+                      8, 7, 0, 4, 4], [8, 7, 4, 0, 2], [8, 7, 4, 2, 0]])
+        matrix = array([[1, 0, 0, 0, 0], [0, 1, 0, 0, 0], [
+                       0, 0, 1, 0, 0], [0, 0, 0, 1, 0], [0, 0, 0, 1, 1]])
 
-        self.assertCorrectPValue(0.0, 0.05, hommola_cospeciation_test, 
-                                                (hdist, pdist, matrix, 1000))
-        
-        self.assertCorrectPValue(0.2, 0.8, hommola_cospeciation_test, 
-                                                (hdist, pdist, randomized_matrix, 1000))
+        # this matrix was picked because it will generate an r value that's less than
+        # a standard deviation away from the mean of the normal distribution of
+        # r vals
+        randomized_matrix = array(
+            [[0, 0, 0, 1, 0], [0, 0, 0, 0, 1], [1, 0, 0, 0, 0], [1, 0, 0, 0, 0], [0, 0, 0, 0, 1]])
 
+        self.assertCorrectPValue(0.0, 0.05, hommola_cospeciation_test,
+                                 (hdist, pdist, matrix, 1000))
+
+        self.assertCorrectPValue(0.2, 0.8, hommola_cospeciation_test,
+                                 (hdist, pdist, randomized_matrix, 1000))
 
     def test_get_dist(self):
-        labels = [0,1,1,2,3]
-        dists = array([[0,2,6,3],[2,0,5,4],[6,5,0,7],[3,4,7,0]])
-        index = [2,3,1,0]
+        labels = [0, 1, 1, 2, 3]
+        dists = array([[0, 2, 6, 3], [2, 0, 5, 4], [6, 5, 0, 7], [3, 4, 7, 0]])
+        index = [2, 3, 1, 0]
 
-        expected_vec = [7,7,5,6,0,4,3,4,3,2]
-        actual_vec = get_dist(labels,dists,index)
+        expected_vec = [7, 7, 5, 6, 0, 4, 3, 4, 3, 2]
+        actual_vec = get_dist(labels, dists, index)
 
         self.assertEqual(actual_vec, expected_vec)
 
-
     def test_distmat_to_tree(self):
         pass
-        
 
 
-
-
-
-
-#run tests if called from command line
+# run tests if called from command line
 if __name__ == "__main__":
     main()
