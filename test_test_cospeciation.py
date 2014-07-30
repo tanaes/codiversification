a9cc986f1a77b3bcd7bb2dcc83407fad6d795e54
Nate Bresnick <ndbresnick@gmail.com>
2014-07-29 20:00:14 -0600

    Fixed tests

--- test_test_cospeciation.py
+++ test_test_cospeciation.py
@@ -47,59 +47,6 @@ from test_cospeciation import (hommola_cospeciation_test,
 
 class TopLevelTests(TestCase):
     """Tests of top-level functions"""
-
-    def setUp(self):
-        """define some top-level data"""
-        self.aln=[]
-        self.aln.append(('SampleA','AAAAAAAAAAAAAAA'))
-        self.aln.append(('SampleB','CCCCCCC'))
-        self.aln.append(('SampleC','GGGGGGGGGGGGGG'))
-        
-        self.otus={'0':['SampleC'],'1':['SampleC','SampleA'],'2':['SampleB',\
-                                                                  'SampleA']}
-
-        self.prefs={}
-        self.prefs={'0':'SampleC','1':'SampleB'}
-
-    def test_take_(self):
-        """process_extract_samples: parses the cmd line and determines which
-        samples should be removed"""
-
-        self.sample_to_extract='SampleA,SampleB'
-        exp1={'0':'SampleA','1':'SampleB'}
-
-        obs1=process_extract_samples(self.sample_to_extract)
-
-        self.assertEqual(obs1,exp1)
-
-    def test_filter_otus(self):
-        """filter_otus: determines which sequences should be removed and
-        generates a new otus list"""
-
-        exp1=[('1',['SampleA']),('2',['SampleA'])]
-        obs1=filter_otus(self.otus,self.prefs)
-            
-        self.assertEqual(obs1,exp1)
-
-        
-    def test_filter_aln_by_otus(self):
-        """filter_aln_by_otus: determines which sequences to keep and which
-        sequences to remove"""
-
-        self.sample_to_extract='SampleA,SampleB'
-        exp1=[]
-        exp1.append(('SampleA','AAAAAAAAAAAAAAA'))
-        exp2=[]
-        exp2.append(('SampleB','CCCCCCC'))
-        exp2.append(('SampleC','GGGGGGGGGGGGGG'))
-        aln=LoadSeqs(data=self.aln,aligned=False)
-
-        obs1,obs2=filter_aln_by_otus(aln,self.prefs)
-
-        self.assertEqual(obs1,exp1)
-        self.assertEqual(obs2,exp2)
-
-
     def test_cogent_dist_to_qiime_dist(self):
 
         input_dict = {('a','b'): 4, ('a','c'): 5, ('a','d'): 6,
@@ -199,8 +146,6 @@ GATTGAACGCTGGCGGCAGGCTTAACACATGCAAGTCGAGCGGGGAATTGTAGCTTGCTACATGACCTAGCGGCGGACGG
 GATTGAACGCTGGCGGCAGGCTTAACACATGCAAGTCGAGCGGGGAATTGTAGCTTGCTACATGACCTAGCGGCGGACGGGTGAGTAATACTTAGGAATCTGCCTATTAGTGGGGGACAACGTTCCGAAAGGAGCGCTAATACCGCATACGCCCTACGGGGGAAAGCAGGGGATCTTCGGACCTTGCGCTAATAGATGAGCCTAAGTCGGATTAGCTAGTTGGTAGGGTAAAGGCCTACCAAGGCGACGATCTGTAGCGGGTTTGAGAGGATGATCCGCCACACTGGGGGGTGAGACACGGCCCAGACTCCTACGGGAGGCAGCAGTGGGGAATATTGGACAATGGGCGCAAGCCTGATCCAGCCATGCCGCGTGTGTGAAGAAGGCCTTATGGTTGTAA---
 """
 
-    def test_distmat_to_tree(self):
-        pass
 
 
 # run tests if called from command line
